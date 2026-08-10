d898bfe69da6af99eec8889c733f2a927ab701fe
Dekate Rishabh <rishabhdekate98@gmail.com>
2026-08-10 09:59:10 +0530

    Restore MDX build support

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,9 @@
+import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
+
+const withMDX = createMDX({
+  extension: /\.mdx?$/,
+});
 
 const nextConfig: NextConfig = {
   experimental: {
@@ -17,4 +22,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default withMDX(nextConfig);

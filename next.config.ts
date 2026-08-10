ecd92fdb82c8f129d1598e9879eae7313574361f
Dekate Rishabh <rishabhdekate98@gmail.com>
2026-08-10 19:52:42 +0530

    Allow Supabase Storage images

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,6 @@
-import createMDX from "@next/mdx";
 import type { NextConfig } from "next";
 
-const withMDX = createMDX({
-  extension: /\.mdx?$/,
-});
-
 const nextConfig: NextConfig = {
-  experimental: {
-    serverActions: {
-      bodySizeLimit: "10mb",
-    },
-  },
   images: {
     remotePatterns: [
       {
@@ -22,4 +12,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withMDX(nextConfig);
+export default nextConfig;
